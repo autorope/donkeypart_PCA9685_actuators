a70b551b8d2b4b955115d410a6f83afdd4fd1763
Will Roscoe <wroscoe@gmail.com>
2018-11-25 17:25:15 -0800

    remove code for differential drive

--- donkeypart_PCA9685_actuators/part.py
+++ donkeypart_PCA9685_actuators/part.py
@@ -95,48 +95,6 @@ class PWMThrottle:
         self.run(0)  # stop vehicle
 
 
-class Adafruit_DCMotor_Hat:
-    """
-    Adafruit DC Motor Controller
-    Used for each motor on a differential drive car.
-    """
-    def __init__(self, motor_num):
-        from Adafruit_MotorHAT import Adafruit_MotorHAT
-        import atexit
-
-        self.FORWARD = Adafruit_MotorHAT.FORWARD
-        self.BACKWARD = Adafruit_MotorHAT.BACKWARD
-        self.mh = Adafruit_MotorHAT(addr=0x60)
-
-        self.motor = self.mh.getMotor(motor_num)
-        self.motor_num = motor_num
-
-        atexit.register(self.turn_off_motors)
-        self.speed = 0
-        self.throttle = 0
-
-    def run(self, speed):
-        """
-        Update the speed of the motor where 1 is full forward and
-        -1 is full backwards.
-        """
-        if speed > 1 or speed < -1:
-            raise ValueError("Speed must be between 1(forward) and -1(reverse)")
-
-        self.speed = speed
-        self.throttle = int(map_range(abs(speed), -1, 1, -255, 255))
-
-        if speed > 0:
-            self.motor.run(self.FORWARD)
-        else:
-            self.motor.run(self.BACKWARD)
-
-        self.motor.setSpeed(self.throttle)
-
-    def shutdown(self):
-        self.mh.getMotor(self.motor_num).run(Adafruit_MotorHAT.RELEASE)
-
-
 def map_range(x, X_min, X_max, Y_min, Y_max):
     """
     Linear mapping between two ranges of values
